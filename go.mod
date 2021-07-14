ae65ff2aaf53316dacf8cd7335c924440b0e4a32
incrypto <vpkrishnanand@gmail.com>
2021-07-14 19:23:42 +0530

    added .env

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/incrypto32/airdropbot
 
 go 1.16
 
-require github.com/labstack/echo/v4 v4.4.0 // indirect
+require (
+	github.com/joho/godotenv v1.3.0 // indirect
+	github.com/labstack/echo/v4 v4.4.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
 
 func main() {
+	log.SetFlags(log.Llongfile)
+
+	// Load ENV FIle
+	if err := godotenv.Load(); err != nil {
+
+		log.Panic("Failed loading env variables", err)
+	}
+
 	fmt.Println("Hello World")
+	fmt.Println(os.Getenv("BOT_TOKEN"))
 }
